b239747eb28de688bcec915140fa35c2f4bcc3be
stfx86 <mustaphabennasser8@gmail.com>
2025-05-22 12:03:15 +0100

    trabsaction flow

--- scripts/getSupplier.js
+++ scripts/getSupplier.js
@@ -6,13 +6,7 @@ const contractAddress ='0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
 const SupplierRegistry = await  ethers.getContractAt('SupplierRegistry',contractAddress);
 const Supplier = await SupplierRegistry.getSupplier("0x2546BcD3c84621e976D8185a91A922aE77ECEc30")
 console.log(Supplier);
-
-
-
-
     
 }
-
-
 
 main();
